c18387a54229ebc22ab2d64f94f04c06cf0c9a5e
OchoaBot <91572024+OchoaBot@users.noreply.github.com>
2021-09-30 18:44:08 -0500

    Add files via upload

---
+++ src/version.js
@@ -0,0 +1,22 @@
+const version = (prefix, pushname) => {
+    return `
+*XavyBot By Ochoa*
+
+*Actualizado:* 18 de Septiembre del 2021
+*Versión actual:* 4.0
+*Ofrecida por:* Ochoa™
+
+
+*INFORME*
+
+Si no funciona el comando !play o !play2 checa el blog del bot y mira la version que poses 
+Ya que ire cambiando mi apikey de mi bot para que siga en funcionamiento
+Cambiare la apikey pasando un mes o si veo muchos atercados o robos
+
+
+_*by Ochoa*_
+`
+
+}
+
+exports.version = version
