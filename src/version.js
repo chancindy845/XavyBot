d6bf58791516b26b54b415dccd4a89e08eecce26
OchoaBot <91572024+OchoaBot@users.noreply.github.com>
2021-10-11 12:12:32 -0500

    Update version.js

--- src/version.js
+++ src/version.js
@@ -2,14 +2,14 @@ const version = (prefix, pushname) => {
     return `
 *XavyBot By Ochoa*
 
-*Actualizado:* 05 De Septiembre del 2021
-*Versión actual:* 5.0
+*Actualizado:* 12 DE OCTUBRE DEL 2021
+*Versión actual:* 1.0
 *Ofrecida por:* Ochoa™
 
 
 *INFORME*
 
-Si no funciona el comando !play o !play2 checa el blog del bot y mira la version que poses 
+Si no sirve el !kick o !pafuera revisa que version del bot tienes y revisa el blog del bot
 Ya que ire cambiando mi apikey de mi bot para que siga en funcionamiento
 Cambiare la apikey pasando un mes o si veo muchos atercados o robos
 
